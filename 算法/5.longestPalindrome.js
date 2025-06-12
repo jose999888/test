5d7074016aadb04f845b9a900a4a8b4aefe78107
jose999888 <168402067+jose999888@users.noreply.github.com>
2025-06-12 13:48:21 +0700

    算法5

--- 算法/5.longestPalindrome.js
+++ 算法/5.longestPalindrome.js
@@ -1,29 +1,32 @@
-
-
-
-
-
-function longestPalindrome(s) {
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    if (s.length<2){
+        return s
+    }
     let res = ''
-    const map = new Map()
-    const len = s.length
-    if(s.length === 1) return s
-    for(let i = 0;i < len;i++){
-        const cur = s[i]
-        if(i > 0 && map.has(cur)) {
-            if(s[i + 1] === s[i - 1] && s[i] === s[i+2]){
-                continue
-            }else {
-                res = s.slice(map.get(cur),i+1)
-            }
+    for (let i = 0; i < s.length; i++) {
+        // 回文子串长度是奇数
+        helper(i, i)
+        // 回文子串长度是偶数
+        helper(i, i + 1)
+    }
+
+    function helper(m, n) {
+        while (m >= 0 && n < s.length && s[m] === s[n]) {
+            m--
+            n++
         }
-        map.set(cur,i)
-        if(!res){
-            res = s[i]
+        // 注意此处m,n的值循环完后  是恰好不满足循环条件的时刻
+        // 此时m到n的距离为n-m+1，但是mn两个边界不能取 所以应该取m+1到n-1的区间  长度是n-m-1
+        if (n - m - 1 > res.length) {
+            // slice也要取[m+1,n-1]这个区间
+            res = s.slice(m + 1, n)
         }
     }
-    return  res
+    return res
 };
-// 0123 4  5 6789
 
-console.log(longestPalindrome('babab'))
+console.log(longestPalindrome('babad'))
